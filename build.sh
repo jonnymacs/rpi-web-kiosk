a106cda7a146e2edec304244627ce4630fcdd010
Jonathan McAllister <jmcallis@gmail.com>
2025-04-30 09:56:04 -0700

    migrate to generic naming convention

--- build.sh
+++ build.sh
@@ -5,21 +5,24 @@ set -eu
 BUILD_ID=${RANDOM}
 BINARY_BUILD_SVC="wifi_setup_service"
 BINARY_NAME="wifi_setup_service"
-RPI_BUILD_SVC="macmind_rpi_web_kiosk"
+RPI_BUILD_SVC="custom-rpi-image"
 RPI_BUILD_USER="imagegen"
-RPI_CUSTOMIZATIONS_DIR="macmind_rpi_web_kiosk"
+RPI_CUSTOMIZATIONS_DIR="custom-rpi-image"
+RPI_CONFIG=${RPI_BUILD_SVC}
+RPI_OPTIONS=${RPI_BUILD_SVC}
 RPI_IMAGE_NAME="macmind_rpi_web_kiosk"
+SAVE_SBOM=1
 
 ensure_cleanup() {
   echo "Cleanup containers..."
 
-  BINARY_BUILD_SVC_CONTAINER_ID=$(docker ps -a --filter "name=${BINARY_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
-    && docker kill ${BINARY_BUILD_SVC_CONTAINER_ID} \
-    && docker rm ${BINARY_BUILD_SVC_CONTAINER_ID}
-
   RPI_BUILD_SVC_CONTAINER_ID=$(docker ps -a --filter "name=${RPI_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
     && docker kill ${RPI_BUILD_SVC_CONTAINER_ID} \
     && docker rm ${RPI_BUILD_SVC_CONTAINER_ID}
+
+  BINARY_BUILD_SVC_CONTAINER_ID=$(docker ps -a --filter "name=${BINARY_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
+    && docker kill ${BINARY_BUILD_SVC_CONTAINER_ID} \
+    && docker rm ${BINARY_BUILD_SVC_CONTAINER_ID}
 
   echo "Cleanup complete."
 }
@@ -27,8 +30,8 @@ ensure_cleanup() {
 # Set the trap to execute the ensure_cleanup function on EXIT
 trap ensure_cleanup EXIT
 
-# build the wifi setup service binary
-# and copy it to the imagegen_ext_dir
+# build the rust binary
+# and copy it to the imagegen ext_dir
 # for the raspberry pi image build
 #
 echo "🔨 Building Docker image to compile ${BINARY_BUILD_SVC}..."
@@ -47,8 +50,13 @@ docker compose build ${RPI_BUILD_SVC}
 
 echo "🚀 Running image generation in container..."
 docker compose run --name ${RPI_BUILD_SVC}-${BUILD_ID} -d ${RPI_BUILD_SVC} \
-  && docker compose exec ${RPI_BUILD_SVC} bash -c "/home/${RPI_BUILD_USER}/rpi-image-gen/build.sh -D /home/${RPI_BUILD_USER}/${RPI_CUSTOMIZATIONS_DIR}/ext_dir -c ${RPI_IMAGE_NAME} -o /home/${RPI_BUILD_USER}/${RPI_CUSTOMIZATIONS_DIR}/ext_dir/${RPI_IMAGE_NAME}.options" \
+  && docker compose exec ${RPI_BUILD_SVC} bash -c "/home/${RPI_BUILD_USER}/rpi-image-gen/build.sh -D /home/${RPI_BUILD_USER}/${RPI_CUSTOMIZATIONS_DIR}/ext_dir -c ${RPI_CONFIG} -o /home/${RPI_BUILD_USER}/${RPI_CUSTOMIZATIONS_DIR}/ext_dir/${RPI_OPTIONS}.options" \
   && CID=$(docker ps -a --filter "name=${RPI_BUILD_SVC}-${BUILD_ID}" --format "{{.ID}}" | head -n 1) \
-  && docker cp ${CID}:/home/${RPI_BUILD_USER}/rpi-image-gen/work/${RPI_IMAGE_NAME}/deploy/${RPI_IMAGE_NAME}.img ./${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).img
+  && docker cp ${CID}:/home/${RPI_BUILD_USER}/rpi-image-gen/work/${RPI_IMAGE_NAME}/deploy/${RPI_IMAGE_NAME}.img ./${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).img \
+
+if [[ "${SAVE_SBOM}" == "1" ]]; then
+  docker cp ${CID}:/home/${RPI_BUILD_USER}/rpi-image-gen/work/${RPI_IMAGE_NAME}/deploy/${RPI_IMAGE_NAME}.sbom ./${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).sbom
+fi
 
 echo "🚀 Completed -> ${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).img"
+echo "🚀 Completed -> ${RPI_CUSTOMIZATIONS_DIR}/deploy/${RPI_IMAGE_NAME}-$(date +%m-%d-%Y-%H%M).sbom"
